d20a76f1c37b35097fbae0628e93e23cb0a1e4a2
Luke Shirnia <lukeshirnia@btinternet.com>
2017-01-17 17:45:26 +0000

    Create os_check.py

---
+++ os_check.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+import platform
+import sys
+
+def os_check():
+        global os_platform, os_distro, os_version
+        os_platform = platform.system()
+
+        if os_platform == "Linux":
+                os_distro = platform.linux_distribution()[0]
+                #os_version = int(float(platform.linux_distribution()[1]))
+                os_version = platform.linux_distribution()[1]
+                print os_distro, os_version
+
+
+os_check()
