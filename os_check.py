c4a61d52ded9caa9b00d58c4ed812e2c4274e50b
Luke Shirnia <lukeshirnia@btinternet.com>
2017-01-17 17:47:08 +0000

    Update os_check.py

--- os_check.py
+++ os_check.py
@@ -11,6 +11,9 @@ def os_check():
                 #os_version = int(float(platform.linux_distribution()[1]))
                 os_version = platform.linux_distribution()[1]
                 print os_distro, os_version
+         else:
+                print "Stop Using A Rubish OS"
+
 
 
 os_check()
