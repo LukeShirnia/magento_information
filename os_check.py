dd57a5889daa33280d01bf3233f43b6dda3949f4
Luke Shirnia <lukeshirnia@btinternet.com>
2017-01-24 10:45:07 +0000

    Update os_check.py

--- os_check.py
+++ os_check.py
@@ -2,18 +2,33 @@
 import platform
 import sys
 
+#global os_platform
+
+def version_check():
+        global os_version, supported_centos, supported_ubuntu, CentOS_RedHat_Distro
+        CentOS_RedHat_Distro = ['CentOS Linux', 'RedHat Linux', 'redhat', 'centos', 'CentOS']
+        Ubuntu_Debian_Distro = ['Ubuntu Linux']
+        supported_centos = [6, 7]
+        supported_ubuntu = [12, 14, 16]
+        os_version = platform.linux_distribution()[1]
+        os_version_test = os_version.split()
+
+        if os_distro in CentOS_RedHat_Distro:
+                print os_distro, os_version
+        elif os_distro in Ubuntu_Debian_Distro:
+                print os_distro, os_version
+        else:
+                print os_distro, os_version
+                print "Something wrong"
+
 def os_check():
-        global os_platform, os_distro, os_version
+        global os_distro, os_version, os_platform
         os_platform = platform.system()
 
         if os_platform == "Linux":
                 os_distro = platform.linux_distribution()[0]
-                #os_version = int(float(platform.linux_distribution()[1]))
-                os_version = platform.linux_distribution()[1]
-                print os_distro, os_version
-         else:
+                version_check()
+        else:
                 print "Stop Using A Rubish OS"
-
-
 
 os_check()
