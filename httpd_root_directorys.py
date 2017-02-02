de0f7efdce963a260a708e2e2efbe2c15c9bf2ba
Luke Shirnia <lukeshirnia@btinternet.com>
2017-02-02 11:16:57 +0000

    Update and rename httpd_root_directorys to httpd_root_directorys.py

--- httpd_root_directorys.py
+++ httpd_root_directorys.py
@@ -1,12 +1,12 @@
 import os
+import sys
 
-def website_configuration():
+def website_configuration(webserver_config):
         config_files=[]
-        webserver_config="/etc/httpd/conf.d/"
         for root, dirs, files in os.walk(webserver_config):
                 for file in files:
                         if file.endswith(".conf"):
                                 config_files.append(os.path.join(root, file))
                 for print_config in config_files: print print_config
 
-website_configuration()
+website_configuration("/etc/httpd/conf.d/")
