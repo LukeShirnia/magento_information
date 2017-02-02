7c5a8c9a3415861271b6353924290801bbb4103b
Luke Shirnia <lukeshirnia@btinternet.com>
2017-02-02 12:37:05 +0000

    Update httpd_root_directorys.py

--- httpd_root_directorys.py
+++ httpd_root_directorys.py
@@ -2,11 +2,21 @@ import os
 import sys
 
 def website_configuration(webserver_config):
+        global config_files
         config_files=[]
         for root, dirs, files in os.walk(webserver_config):
                 for file in files:
                         if file.endswith(".conf"):
                                 config_files.append(os.path.join(root, file))
-                for print_config in config_files: print print_config
+
+def document_root(files_to_search):
+        DocRoots=[]
+        for i in files_to_search:
+                with open(i, "r") as search_file:
+                        for line in search_file:
+                                if 'DocumentRoot' in line:
+                                        DocRoots.append(line)
+        for print_doc_root in DocRoots: print print_doc_root
 
 website_configuration("/etc/httpd/conf.d/")
+document_root(config_files)
