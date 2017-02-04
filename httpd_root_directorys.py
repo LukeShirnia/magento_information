d451b5c66f8507786ea7cb69ffa5a1c48d1f4e78
Luke Shirnia <lukeshirnia@btinternet.com>
2017-02-04 19:55:35 +0000

    Update httpd_root_directorys.py

--- httpd_root_directorys.py
+++ httpd_root_directorys.py
@@ -1,24 +1,46 @@
 import os
 import sys
 import re
+import copy
 
 def http_includes():
-	config_directory=[]
-	server_root=[]
+        global server_root, config_directory
+        config_directory=[]
+        server_root=[]
+        SUFFIX=".conf"
         CONFIG_DIR=[]
-	PATH=[]
-	include = re.compile("^\s*include")
-	serverroot = re.compile("^\s*serverroot")
-	with open("/etc/httpd/conf/httpd.conf", "r") as search_file:
-		for line in search_file:
-	        	if include.match(line.lower()):
+        PATH=[]
+        include = re.compile("^\s*include")
+        serverroot = re.compile("^\s*serverroot")
+
+        with open("/etc/httpd/conf/httpd.conf", "r") as search_file:
+                for line in search_file:
+                        if include.match(line.lower()):
                                 line = line.split(" ")[1]
+#                               suffix_match = line.split("*")[1]
+                                #SUFFIX = [x.replace('*','') for x in SUFFIX]
                                 CONFIG_DIR = line.split("/")[0]
                                 config_directory.append(CONFIG_DIR)
-			if serverroot.match(line.lower()):
+#                               SUFFIX.append(suffix_match)
+                        if serverroot.match(line.lower()):
                                 line = line.split('"')[1]
                                 server_root.append(line)
                                 server_root = [x.replace("ServerRoot", "") for x in server_root]
+
+def http_vhost_directory_fullpath(file_root, docs_directory):
+	global vhost_directory_path
+ 	PATH = []
+	vhost_direcory_path=[]
+
+	for i in docs_directory:
+        	# since file_root can be an array, use copy to grab a copy 
+        	# of the array
+        	args = copy.copy(file_root)
+        	args.append(i)
+        	# and stick `i` on the end of that array so
+        	# that we have our full param list for os.path.join
+        	PATH = os.path.join(*args)
+        	vhost_directory_path.append(PATH)
 		
 def website_configuration(webserver_config, config_suffix):
         global config_files
@@ -42,3 +64,5 @@ def document_root(files_to_search):
 
 website_configuration("/etc/httpd/conf.d/", ".conf")
 document_root(config_files)
+http_vhost_directory_fullpath(server_root, config_directory)
+print vhost_directory_path[0]
