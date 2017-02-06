53245ba4bdfe03681b627aec90363ae4ecb4afb9
Luke Shirnia <lukeshirnia@btinternet.com>
2017-02-06 08:31:04 +0000

    Update not_working_function.py

--- not_working_function.py
+++ not_working_function.py
@@ -12,11 +12,9 @@ def website_configuration(webserver_config, config_suffix):
         global config_files
         config_files = []
 	for i in webserver_config:
-		print i
 	        for root, dirs, files in os.walk(i):
                 	for file in files:
                         	if file.endswith(config_suffix):
                                 	#config_files.append(os.path.join(root, file))
-
 
 website_configuration(config_directories, suffix)
