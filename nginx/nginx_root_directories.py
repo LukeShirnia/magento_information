3638838db05ec483b986d79f0186da1bd914723c
luke7858 <lukeshirnia@btinternet.com>
2017-02-27 08:13:57 +0000

    initial commit

---
+++ nginx/nginx_root_directories.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import re
+import copy
+
+
+def print_header():
+        print "-" * 60
+        print ""
+        print " _____ _____ _____ _____ _____ _____ _____"
+        print "|     |  _  |  ___|   __|   | |_   _|     |"
+        print "| | | |     | |_| |   __| | | | | | |  |  |"
+        print "|_|_|_|__|__|_____|_____|_|___| |_| |_____|"
+        print ""
+        print "-" * 60
+        print ""
+
+
+
