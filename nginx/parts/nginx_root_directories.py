e61a00e221b099e7a62a5259be1dee21626cba7b
luke7858 <lukeshirnia@btinternet.com>
2017-02-27 14:33:36 +0000

    initial commit

--- nginx/parts/nginx_root_directories.py
+++ nginx/parts/nginx_root_directories.py
@@ -19,7 +19,16 @@ def get_nginx_includes():
                                 line = line.split(" ")[1]
                                 CONFIG_DIR_NGINX = line
                                 config_directory_nginx.append(CONFIG_DIR_NGINX)
+				# remove all empty array entries
 				config_directory_nginx = filter(None, config_directory_nginx)
-				print config_directory_nginx
+				# remove ; from directory path
+				config_directory_nginx = [x.replace(';', "") for x in config_directory_nginx]
+	# find the suffix for each directory and then remove it from the array of includes	
+	suffix_nginx = config_directory_nginx[1]
+
+	# probably need to loop the suffix through all includes instead of manually selecting the first array entry
+	suffix_nginx = suffix_nginx.split('/')[-1]
+	config_directory_nginx = [ remove_nginx_suffix.replace(suffix_nginx, "") for remove_nginx_suffix in config_directory_nginx ] 
+	print config_directory_nginx
 
 get_nginx_includes()
